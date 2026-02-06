033993ba48b714075c1da5a686f1cc57b68f296f
EnsembleTesting <jingyuz042@gmail.com>
2026-02-06 13:27:00 +0800

    Update driving_models_dave2.py

--- adv-self-driving/learner/driving_models_dave2.py
+++ adv-self-driving/learner/driving_models_dave2.py
@@ -368,8 +368,8 @@ class Dave2v1(Classifier):
                 selected_candidateX, selected_candidatey = ensemble_simple(sess, candidateX, candidatey, self, budget, our_method_params)
             elif metric == 'ensemble_simple_add':
                 selected_candidateX, selected_candidatey = ensemble_simple_add(sess, candidateX, candidatey, self, budget, our_method_params)
-            elif metric == 'ensemble_p_values':
-                selected_candidateX, selected_candidatey = ensemble_p_values(sess, trainX, trainy, candidateX, candidatey, self, budget, our_method_params)
+            elif metric == 'ensemble_p_values_fisher':
+                selected_candidateX, selected_candidatey = ensemble_p_values_fisher(sess, trainX, trainy, candidateX, candidatey, self, budget, our_method_params)
             elif metric == 'dat_ood_detector':
                 selected_candidateX, selected_candidatey = dat_ood_detector(sess, trainX, trainy, candidateX, candidatey,
                                                                      hybrid_test,
@@ -413,4 +413,5 @@ def test_dave():
     model = Dave2v1(hyper_params=hyper_param)
     model.test(test_xs, test_ys)
 if __name__ == '__main__':
+
     test_dave()
